0dccde072ea826adb16ae802e62cbff0748481ea
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-06-21 06:18:50 +0200

    add some state to the product list

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Product } from "../Product";
 
-export const ProductsList = () => {
-  return (
-    <div className="container mx-auto grid xl:grid-cols-4 md:grid-cols-3 gap-4 pt-4 pb-12">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </div>
-  );
+export const ProductsList = ({ products }) => {
+  if (products.length === 0) return <h1>No items found.</h1>;
+  else
+    return (
+      <div className="container mx-auto grid xl:grid-cols-4 md:grid-cols-3 gap-4 pt-4 pb-12">
+        {products.map((product, index) => (
+          <Product key={index} data={product} />
+        ))}
+      </div>
+    );
 };
