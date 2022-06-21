33bdba41f8f95ec996f61a87491f10ab23fb252d
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-06-21 06:16:23 +0200

    move the + sign to the right

--- src/components/AddToCartBtn/CounterBtns.jsx
+++ src/components/AddToCartBtn/CounterBtns.jsx
@@ -4,17 +4,17 @@ export const CounterBtns = ({ handleIncrment, handleDecrment, itemCount }) => {
   return (
     <div className="rounded-sm border-purple-600 text-center inline-block border font-bold mb-2">
       <button
-        onClick={handleIncrment}
+        onClick={handleDecrment}
         className="mr-8 p-2 px-4 hover:bg-purple-600 hover:text-white border-r duration-500"
       >
-        +
+        -
       </button>
       {itemCount}
       <button
-        onClick={handleDecrment}
-        className="ml-8 p-2 px-4 hover:bg-purple-600 hover:text-white border-r duration-500"
+        onClick={handleIncrment}
+        className="ml-8 p-2 px-4 hover:bg-purple-600 hover:text-white border-l duration-500"
       >
-        -
+        +
       </button>
     </div>
   );
