37c8f86528f2a668119ce2713971386973f444b0
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-06-22 07:43:05 +0200

    removing unnecessary id attribute

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav id="header" className="w-full z-30 top-0 py-1">
+    <nav className="w-full z-30 top-0 py-1">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
         <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
           <svg
@@ -28,9 +28,17 @@ export const NavBar = () => {
               <li>
                 <Link
                   className="inline-block no-underline hover:text-black hover:underline py-2 px-4"
-                  to="/"
+                  to="/add"
                 >
-                  Shop
+                  Add Product
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="inline-block no-underline hover:text-black hover:underline py-2 px-4"
+                  to="/register"
+                >
+                  Register
                 </Link>
               </li>
               <li>
@@ -64,22 +72,27 @@ export const NavBar = () => {
         </div>
 
         <div className="order-2 md:order-3 flex items-center" id="nav-content">
-          <a className="inline-block no-underline hover:text-black" href="/">
+          <Link
+            to="/favourite"
+            className="inline-block no-underline hover:text-black"
+          >
             <svg
-              className="h-6 w-6 fill-current hover:text-purple-600"
+              className="h-6 w-6 fill-current text-red-600 hover:text-black"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="2.027 3.037 19.945 17.994"
             >
               <path d="M12 4.595 C 10.896 3.589 9.488 3.037 8.004 3.037 C 6.426 3.037 4.932 3.66 3.791 4.795 C 1.438 7.158 1.439 10.854 3.793 13.207 L 11.125 20.539 C 11.295 20.838 11.623 21.031 12 21.031 C 12.322 21.031 12.609 20.868 12.792 20.622 L 20.207 13.207 C 22.561 10.853 22.561 7.158 20.205 4.791 C 19.068 3.66 17.574 3.037 15.996 3.037 C 14.513 3.037 13.104 3.589 12 4.595 Z"></path>
             </svg>
-          </a>
-
-          <a
+            <span className="text-xs absolute right-20 bg-red-600 text-white border-white border-2  top-9 p-[5px] px-[10px] rounded-full">
+              4
+            </span>
+          </Link>
+          <Link
             className="pl-3 inline-block no-underline hover:text-black"
-            href="/"
+            to="/cart"
           >
             <svg
-              className="fill-current hover:text-black"
+              className="fill-current hover:text-black z-50"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -89,7 +102,10 @@ export const NavBar = () => {
               <circle cx="10.5" cy="18.5" r="1.5" />
               <circle cx="17.5" cy="18.5" r="1.5" />
             </svg>
-          </a>
+            <span className="text-xs absolute right-10 bg-purple-600 text-white border-white border-2  top-9 p-[5px] px-[10px] rounded-full">
+              4
+            </span>
+          </Link>
         </div>
       </div>
     </nav>
