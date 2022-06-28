95eb9c6cfe5fb7fa5583c41faa3c761306c31bd8
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-06-28 08:11:29 +0200

    Updates

---
+++ precache-manifest.58431df2dae120de803de82c77220835.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820365dd0a5a749dab57d65847ddb6eb",
+    "url": "/shostore/index.html"
+  },
+  {
+    "revision": "f3d06ed162b61951fd37",
+    "url": "/shostore/static/css/main.65478444.chunk.css"
+  },
+  {
+    "revision": "4f2e5b201c44836dbf6e",
+    "url": "/shostore/static/js/2.d80f7286.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/shostore/static/js/2.d80f7286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d06ed162b61951fd37",
+    "url": "/shostore/static/js/main.0504b567.chunk.js"
+  },
+  {
+    "revision": "0bae428efe9ef71fb190",
+    "url": "/shostore/static/js/runtime-main.107909fc.js"
+  },
+  {
+    "revision": "03b05744228fcdbafaa19de8b917892d",
+    "url": "/shostore/static/media/vapor-cage-t(1).03b05744.png"
+  },
+  {
+    "revision": "00ae4367c9beb7f4a21fe0c40a44e85c",
+    "url": "/shostore/static/media/vapor-cage-t(2).00ae4367.png"
+  },
+  {
+    "revision": "b64d3cbeebf7332c8c0a8db95caf49cc",
+    "url": "/shostore/static/media/vapor-cage-t(3).b64d3cbe.png"
+  },
+  {
+    "revision": "64271248e5a5620976a94399361ea2d1",
+    "url": "/shostore/static/media/vapor-cage-t(4).64271248.png"
+  },
+  {
+    "revision": "9775f5644895d920ecd6222bdfda8740",
+    "url": "/shostore/static/media/vapor-cage-t(5).9775f564.png"
+  },
+  {
+    "revision": "9ecce8fbbe28c1f2585c55a402eb3ed7",
+    "url": "/shostore/static/media/vapor-cage-t(6).9ecce8fb.png"
+  }
+]);
